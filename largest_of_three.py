e5849bb1ba106d81450a610db1955b41b543c370
shazebkhan02 <116637433+shazebkhan02@users.noreply.github.com>
2025-10-26 11:53:52 +0530

    Create largest_of_three.py

---
+++ largest_of_three.py
@@ -0,0 +1,9 @@
+# largest_of_three.py
+# Find the largest among three numbers
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+largest = max(a, b, c)
+print(f"The largest number is {largest}")
